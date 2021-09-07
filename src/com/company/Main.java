34ab6f083d2ecbcb92fe95942a7b66f0a2081fc4
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2021-09-07 07:58:06 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int hour = 11;
+        int minute = 59;
+        String day = "Tuesday";
+        int month =  
+
+        , date, month, and year
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
+    }
+}
